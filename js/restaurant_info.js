5053acdf96bda5606d6c352549dd98fbee5ff87e
Nathalie Marquesin Risbakk <nmarquesin@users.noreply.github.com>
2019-09-08 22:53:53 -0300

    Get map on screen
    Add API keys to js/main.js and js/restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibWFycXVlc2luIiwiYSI6ImNrMGJxenY2dzB4NWYzYnBsMjJ3bG81M3oifQ.mKA3bUy0OKWm3uNPFMiJ4g', // my API key
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
